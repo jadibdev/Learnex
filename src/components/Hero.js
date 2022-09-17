eedfec389056f44007e2a908a3193d31d9593d1e
Mohamed Jadib <jadibdev@gmail.com>
2022-09-17 09:48:01 +0100

    Create react app, install tailwind, test css

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className="w-screen h-96 bg-[#6906FF]">
+      <div className="flex justify-between">
+        <p>logo</p>
+        <p>menu</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
